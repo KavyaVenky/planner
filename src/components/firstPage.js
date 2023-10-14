0a9dbb9fc7e7d043d2de0b785dc98ba40b12c795
Kavya Venky <36632282+KavyaVenky@users.noreply.github.com>
2023-10-13 23:31:29 -0400

    new form updates.

--- src/components/firstPage.js
+++ src/components/firstPage.js
@@ -6,44 +6,46 @@ function FirstPage() {
     currenttime = new Date().toDateString();
     const [date, setNewDate] = useState('');
     const [task, setTask] = useState('');
-    const [time, setDiffTime] = useState(0.0);
+    const [taskArray, setTaskArray] = useState([]);
 
     function obtainDate(event){
-        // splitting the string format array
-        const dateArray = event.target.value.split("-");
-        console.log(dateArray)
-        // create a new date with the separate values
-        var newDate = new Date(dateArray[0], dateArray[1]-1, dateArray[2]);
-        // set date in use state
-        setNewDate(newDate.toDateString());
-        // logging the created date and the date in the use state
-        console.log(newDate.toDateString());
-        console.log(date);
-        // logging the time difference between the two dates
-        console.log(Math.abs(newDate - new Date()) / 36e5);
-        const difftime = Math.abs(newDate - new Date()) / 36e5;
-        setDiffTime(Math.round(difftime));
-
+        setNewDate(event.target.value);
     }
 
     function getTask(event){
         setTask(event.target.value);
     }
 
+    function handleSubmit(event){
+        event.preventDefault();
+        const data = {date, task};
+        if (date && task){
+            setTaskArray((t) => [...taskArray, data]);
+        }
+        setNewDate('');
+        setTask('');
+        console.log("task array elements", taskArray);
+    }
+
     return(
         <>
         <h1>Planner</h1>
-        <div>
-            <label>Enter date: </label> <input type="date" onChange={obtainDate} className="box"/>
-            <label>Enter Task name: </label> <input type="text" onChange={getTask} className="box"/>
-        </div>            
-
-        {/* <p>{currenttime}</p> */}
-        <div className="card">
-            <p>Task name: {task}</p>
-            <p>Date: {date}</p>
-            <p>Time Difference: {time}</p>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Enter date: </label> <input type="date" value={date} onChange={obtainDate} className="box"/>
+                <label>Enter Task name: </label> <input type="text" value={task}onChange={getTask} className="box"/>
+                <button className="btn">Submit</button>
+            </div>            
+        </form>
+        
+        {taskArray.map((t) =>
+            <div className="card">
+                <p>Task name: {t.task}</p>
+                <p>Date: {t.date}</p>
+            </div>
+        )}
+        
+        
         </>
     );
 }
