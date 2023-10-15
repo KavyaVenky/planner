71db2b794b1f37f8fb7210b672d5b8735182ab09
Kavya Venky <36632282+KavyaVenky@users.noreply.github.com>
2023-10-15 16:26:13 -0400

    adding a delete functionality

--- src/components/firstPage.js
+++ src/components/firstPage.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import './firstPage.css';
+import {FaTrash} from "react-icons/fa"
 
 function FirstPage() {
     let currenttime = "";
@@ -27,22 +28,35 @@ function FirstPage() {
         console.log("task array elements", taskArray);
     }
 
+    function handleDelete(event){
+        taskArray.splice(event.target.value, 1);
+        setTaskArray((ls) => [...ls, taskArray]);
+    }
+
     return(
         <>
         <h1>Planner</h1>
         <form onSubmit={handleSubmit}>
-            <div>
+            <div className="form-div">
                 <label>Enter date: </label> <input type="date" value={date} onChange={obtainDate} className="box"/>
                 <label>Enter Task name: </label> <input type="text" value={task}onChange={getTask} className="box"/>
                 <button className="btn">Submit</button>
             </div>            
         </form>
         
-        {taskArray.map((t) =>
-            <div className="card">
-                <p>Task name: {t.task}</p>
-                <p>Date: {t.date}</p>
-            </div>
+        {taskArray.map((t, ind) => {
+            if(t.task && t.date){
+                return(
+                    <div key={ind} className="card">   
+                        <div className="card-header">
+                            <button style={{backgroundColor:"chocolate", border:"none"}} value={ind} onClick={handleDelete}><FaTrash className="delete" size={22}/></button>
+                        </div>                 
+                        <p>Task name: {t.task}</p>
+                        <p>Date: {t.date}</p>
+                    </div>
+                );
+            }
+        }
         )}
         
         
